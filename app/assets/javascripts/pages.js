ab22d8c10b3c21cb8c342822e7d3f48b18471ad0
fabienoger <fab-oger@live.fr>
2015-10-23 14:45:11 +0200

    slider(WIP), container remplaced by container-fluid

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,3 +1,58 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function()
+{
+  // ##### Get all elements #####
+  var $parterns = document.getElementById("carousel-parterns");
+  var $next = document.getElementById("next");
+  var $previous = document.getElementById("previous");
+
+  // ##### Initialize variables #####
+
+  n = 0;
+  autoSlide = "";
+  nbChild = $parterns.children.length;
+
+  console.log($parterns.offsetWidth);
+  console.log(nbChild);
+  console.log($parterns.offsetWidth / 400);
+  // ##### Moove functions #####
+
+  function next()
+  {
+    stopSlide();
+    console.log(n);
+    if (n > ($parterns.offsetWidth / 400)) {n = -1};
+    n++;
+    $parterns.children[0].style.marginLeft = n * -$parterns.offsetWidth + 'px';
+    startInterval();
+  }
+
+  function previous()
+  {
+    stopSlide();
+    if (n == 0) {n = 3};
+    n--;
+    $parterns.children[0].style.marginLeft = n * -$parterns.offsetWidth + 'px';
+    startInterval();
+  }
+
+  // ##### Action functions #####
+
+  function startInterval()
+  {
+    autoSlide = setInterval(next, 3000);
+  }
+
+  function stopSlide() {
+    clearInterval(autoSlide);
+  }
+
+  // ##### Calling functions #####
+  if ($parterns != null || $next != null || $previous != null)
+  {
+    startInterval();
+    $parterns.onmouseover = stopSlide;
+    $parterns.onmouseout = startInterval;
+    $next.onclick = next;
+    $previous.onclick = previous;
+  }
+});
